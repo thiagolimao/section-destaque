98b9f10637eea764cc816eb229772ee7f62756a2
Thiago Limão <thiago@thiagolima.com>
2022-07-26 21:27:16 -0300

    Responsive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,35 +1,23 @@
 !(function ($) {
     "use strict";
 
-    // Preloader
-    $(window).on('load', function () {
-        if ($('#preloader').length) {
-            $('#preloader').delay(100).fadeOut('slow', function () {
-                $(this).remove();
-            });
-        }
-    });
-
     // carousel
-    function owlInfinity() {
-        $(".infinity-carousel").owlCarousel({
+    function courseInfinity() {
+        $(".course-home-carousel").owlCarousel({
             // loop: !0,
             // nav: true,
             // dots: !1,
             // loop: true,
             // margin: 10,
-            nav: true,
-            margin: 26,
+            nav: false,
+            margin: 15,
             responsive: {
                 0: { items: 1.25 },
-                600: { items: 2.15 },
-                768: { items: 2.25 },
-                900: { items: 2.5 },
-                1024: { items: 3 },
-                1100: { items: 3 },
-                1200: { items: 3.15 },
-                1350: { items: 3.25 },
-                1500: { items: 4 },
+                460: { items: 2.15 },
+                576: { items: 2.5 },
+                690: { items: 3.15 },
+                768: { items: 3.15 },
+                900: { items: 4.15 }
             },
         });
     }
@@ -44,7 +32,7 @@
 
     $(window).on('load', function () {
         aos_init();
-        owlInfinity();
+        courseInfinity();
     });
 
 })(jQuery);
